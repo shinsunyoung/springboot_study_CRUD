8720ea729a0d4bfe0640cf94f285d64714d93214
Sunyoung Shin <s2017i30@e-mirim.hs.kr>
2019-04-03 23:36:25 +0900

    [repository] PersonRepository 추가

---
+++ src/main/java/com/shinsunyoung/springbootcrud/repository/PersonRepository.java
@@ -0,0 +1,10 @@
+package com.shinsunyoung.springbootcrud.repository;
+
+import com.shinsunyoung.springbootcrud.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+}
